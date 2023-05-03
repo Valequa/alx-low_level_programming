301ffc80376898c59e23cc439f0ca0f539772b46
valequa <valeriemorsi@yahoo.com>
2023-05-03 12:51:38 -0700

    11

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,29 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
-/**
-* _strcpy - copies the string pointed to by src,
-* including the terminating null byte, to the
-* buffer pointed to by dest.
-* @dest: destination.
-* @src: source.
-* Return: the pointer to dest.
-*/
-char *_strcpy(char *dest, char *src)
-{
-int count = 0;
-while (count >= 0)
-{
-*(dest + count) = *(src + count);
-if (*(src + count) == '\0')
-break;
-count++;
-}
-return (dest);
-}
-
-Advanced task 1
-#include "main.h"
 
 /**
  * _atoi - converts a string to an integer.
